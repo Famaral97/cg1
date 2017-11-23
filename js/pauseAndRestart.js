4d9972f3039ffb629e267275ee511c5c005892b1
joaoteixeira97 <32222467+joaoteixeira97@users.noreply.github.com>
2017-11-23 21:51:07 +0000

    caixa magica

--- js/pauseAndRestart.js
+++ js/pauseAndRestart.js
@@ -5,14 +5,16 @@ var isPaused, isOver;
 isPaused = false;
 isOver = false;
 
-var transparentMaterial=new THREE.MeshBasicMaterial( { color: 0xffffff ,opacity: 0.0,transparent:true} );
+var transparentMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff ,opacity: 0.0,transparent:true} );
+var pause = new THREE.MeshBasicMaterial( {transparent: true, map: new THREE.TextureLoader().load("textures/pause.png")});
+var gameover = new THREE.MeshBasicMaterial( {transparent: true, map: new THREE.TextureLoader().load("textures/gameover.png")});
 
 function renderTexture() {
   if (isOver) {
-    letterBox.children[0].material = new THREE.MeshBasicMaterial( { color: 0xffffff,opacity: 0.0, transparent:true, map: new THREE.TextureLoader().load('textures/gameover.png') } );
+    letterBox.children[0].material = gameover;
   } else if (isPaused) {
-    letterBox.children[0].material = new THREE.MeshBasicMaterial( {  map: new THREE.TextureLoader().load('textures/pause.png') } );
-    
+  
+    letterBox.children[0].material = pause;
   } else {
     letterBox.children[0].material = transparentMaterial;
   }
@@ -23,7 +25,7 @@ function createBox(x, y, z) {
 
   letterBox = new THREE.Object3D();
 
-  var geometry = new THREE.BoxGeometry(1500, 1  , 1000);
+  var geometry = new THREE.BoxGeometry(1000, 1  , 750);
 
   var mesh = new THREE.Mesh(geometry, transparentMaterial);
 
